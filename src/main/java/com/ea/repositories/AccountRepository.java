a66f98c60efe476c6d60248889a696a0c8a86276
a-blondel <ablondel@protonmail.com>
2025-01-20 22:25:52 +0100

    Duplicate check for account and persona is now case insensitive

--- src/main/java/com/ea/repositories/AccountRepository.java
+++ src/main/java/com/ea/repositories/AccountRepository.java
@@ -2,11 +2,16 @@ package com.ea.repositories;
 
 import com.ea.entities.AccountEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface AccountRepository extends JpaRepository<AccountEntity, Long> {
-    Optional<AccountEntity> findByName(String name);
+
+    @Query("SELECT a FROM AccountEntity a WHERE LOWER(a.name) = LOWER(:name)")
+    Optional<AccountEntity> findByName(@Param("name") String name);
+
 }
